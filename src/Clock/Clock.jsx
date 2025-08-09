01cca47fd8a516eb565b2ee40b0df3952c29ba01
Artem Sokolov <the.jelll.2006@gmail.com>
2025-08-09 11:32:33 +0500

    add component 'clock'

---
+++ src/Clock/Clock.jsx
@@ -0,0 +1,14 @@
+import './Clock.scss'
+import {useState} from "react";
+
+const Clock = () => {
+  const [nowTime, setNowTime] = useState(new Date())
+  setInterval(() => setNowTime(new Date()),1000)
+  return (
+    <div className='clock'>
+      <span className="clock__span">{nowTime.toLocaleTimeString()}</span>
+    </div>
+  )
+}
+
+export default Clock
